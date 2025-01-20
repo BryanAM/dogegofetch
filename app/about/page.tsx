19db8640019cdc3dfe6d421fd3c5d8b19a121628
Bryan Aument <bryaument@gmail.com>
2025-01-19 23:05:17 -0500

    added attribution to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
+import dog from "../../public/assets/shiba-inu-icon.svg";
+import Link from "next/link";
 export default function About() {
-  return <div>about</div>;
+  return (
+    <main>
+      <h1>About Doge Go Fetch</h1>
+
+      <h2>Built With</h2>
+
+      <h2>Attributions</h2>
+      <Image src={dog} alt="shiba inu" />
+      <Link
+        href="https://thenounproject.com/icon/shiba-inu-1603338/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Shiba Inu icon by Kelly Scruggs from Noun Project CC BY 3.0{" "}
+      </Link>
+    </main>
+  );
 }
