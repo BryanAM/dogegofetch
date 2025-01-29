9ba70a22c6237a485d131207497def67ca4d88a3
Bryan Aument <bryaument@gmail.com>
2025-01-29 12:08:21 -0500

    added sidebar to examples page, to do populating with info

--- app/examples/layout.tsx
+++ app/examples/layout.tsx
@@ -1,7 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="flex flex-col">{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
